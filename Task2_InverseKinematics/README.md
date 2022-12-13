a5744639fa86b5778ab4c2af4dfbba132b8d5571
gabrisyra <gabristomislav@gmail.com>
2022-12-13 13:55:35 +0100

    update with source files

--- Task2_InverseKinematics/README.md
+++ Task2_InverseKinematics/README.md
@@ -1,1 +1,1 @@
-
+Due to the vitis HLS being poorly made, you will have to upload the source and simulation files independently from the project (Project > add source/testbench)
--- Task2_InverseKinematics/inversekinematics/solution1/script.tcl
+++ Task2_InverseKinematics/inversekinematics/solution1/script.tcl
@@ -5,9 +5,9 @@
 ############################################################
 open_project inversekinematics
 set_top inversekinematics
-add_files ../cpp/inversekinematics.cpp
-add_files ../cpp/inversekinematics.h
-add_files -tb ../cpp/inversekinematics_test.cpp
+add_files inversekinematics.h
+add_files inversekinematics.cpp
+add_files -tb inversekinematics_test.cpp -cflags "-Wno-unknown-pragmas" -csimflags "-Wno-unknown-pragmas"
 open_solution "solution1" -flow_target vivado
 set_part {xczu3eg-sbva484-1-i}
 create_clock -period 10 -name default
