cbcf74a1d37224e0c25d853f542a86336de187f5
Zain Ahmed <zaahm18@student.sdu.dk>
2022-12-13 14:30:41 +0100

    update

---
+++ Task3_ROSToMotors/build/dynamixel_sdk/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/zain/Documents/1st_semester/Embedded_Systems/Assignments/MiniProject/EmbeddedSystem-miniProject/Task3_ROSToMotors/src/dynamixel_sdk
+# Build directory: /home/zain/Documents/1st_semester/Embedded_Systems/Assignments/MiniProject/EmbeddedSystem-miniProject/Task3_ROSToMotors/build/dynamixel_sdk
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ Task3_ROSToMotors/build/dynamixel_sdk_custom_interfaces/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/zain/Documents/1st_semester/Embedded_Systems/Assignments/MiniProject/EmbeddedSystem-miniProject/Task3_ROSToMotors/src/dynamixel_sdk_custom_interfaces
+# Build directory: /home/zain/Documents/1st_semester/Embedded_Systems/Assignments/MiniProject/EmbeddedSystem-miniProject/Task3_ROSToMotors/build/dynamixel_sdk_custom_interfaces
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("dynamixel_sdk_custom_interfaces__py")
